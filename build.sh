3ca8bf2ee18a6bb9f71c36a5f39ef98b4abf507d
gjbex <geertjan.bex@uhasselt.be>
2016-08-25 09:32:04 +0200

    Added cluster specific build instructions

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -l
 
-source switch_to_2015a
-module load intel/2015a
+case ${VSC_INSTITUTE_CLUSTER} in
+    thinking)
+        source switch_to_2015a
+        module load intel/2015a
+        ;;
+    breniac)
+        module load intel/2016a
+        ;;
+esac
 
 ./configure CC=mpiicc --prefix="${HOME}/tmp/usr"
 
