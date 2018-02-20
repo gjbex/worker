a994edf05886526e11900ccca3801f4531734c14
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-02-20 07:47:19 +0100

    Adapt for OpenMPI build

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 case ${VSC_INSTITUTE_CLUSTER} in
     thinking)
         source switch_to_2015a
-        module load intel/2015a
+        module load foss/2015a
         ;;
     breniac)
-        module load intel/2016a
+        module load foss/2016a
         ;;
 esac
 
-./configure CC=mpiicc --prefix="${HOME}/tmp/usr"
+./configure CC=mpicc --prefix="${HOME}/tmp/usr"
 
 if [ $? ]
 then
